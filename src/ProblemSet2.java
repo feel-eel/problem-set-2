42d89f07240139b6a8c1ab526c28eecae5022878
Julia Ellis <juliaellis101@gmail.com>
2018-09-28 09:08:49 -0400

    finsihed pset2

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * INSTRUCTIONS.
  * 
@@ -17,30 +19,104 @@ public class ProblemSet2 {
 	public static void main (String[] args) {
 		ProblemSet2 ps2 = new ProblemSet2();
 		
-		ps2.sayHello();
-		ps2.gradeMe();
+		//ps2.sayHello();
+		//ps2.gradeMe();
 		ps2.groupUs();
-		ps2.info();
-		ps2.initials();
+		//ps2.info();
+		//ps2.initials();
 	}
 	
 	public void sayHello() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What's your first name?");
+		String fName = in.nextLine();
 		
+		System.out.println("What's your last name?");
+		String lName = in.nextLine();
+		
+		System.out.println("Hello " + fName + " " + lName + "!" );
 	}
 	
 	public void gradeMe() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What's your homework grades?");
+		double homeworkone = in.nextDouble();
+		double homeworktwo = in.nextDouble();
+		double homeworkthree = in.nextDouble();
+
 		
+		System.out.println("What's your quiz grades?");
+		double quizone = in.nextDouble();
+		double quiztwo = in.nextDouble();
+		double quizthree = in.nextDouble();
+		
+		
+		System.out.println("What's your test grades?");
+		double testone = in.nextDouble();
+		double testtwo = in.nextDouble();
+		double testthree = in.nextDouble();
+		
+		
+		double finalGrade = (((homeworkone + homeworktwo + homeworkthree) / 3) * .15) +
+				+ (((quizone + quiztwo + quizthree) /3) * .30)
+				+ (((testone + testtwo + testthree) /3) * .55);
+		System.out.println("Your final grade is " + finalGrade);
 	}
 	
 	public void groupUs() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("How many teachers are going on the trip?");
+		int teachers = in.nextInt();
 		
+		System.out.println("How many students are going on the trip?");
+		int bus;
+		int students = in.nextInt();
+		int remainder = (teachers + students) % 47;
+		bus = ((teachers + students) % 47 == 0) ?  ((teachers + students)/ 47) : ((teachers + students)/47 + 1); 
+		if ((teachers + students) % 47 == 0) {
+			System.out.println("You need " + bus + " buses. The buses will all have 47 people.");
+		} else {
+		System.out.println("You need " + bus + " buses. The last bus will have " + remainder + " people.");
+		}
+
 	}
 	
 	public void info() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("What's your first name?");
+		String fName = in.nextLine();
 		
+		System.out.println("What's your last name?");
+		String lName = in.nextLine();
+		
+		System.out.println("What's your grade?");
+		String grade = in.nextLine();
+		
+		System.out.println("What's your age?");
+		String age = in.nextLine();
+		
+		System.out.println("What's your hometown?");
+		String hometown = in.nextLine();
+		
+		System.out.println("NAME     : " + fName + " " + lName);
+		System.out.println("GRADE    : " + grade); 
+		System.out.println("AGE      : " + age);
+		System.out.println("HOMETOWN : " + hometown);
+		System.out.println(" ");
 	}
 	
 	public void initials() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter your first name: ");
+		String fName = in.nextLine();
 		
+		System.out.print("Please enter your middle name: ");
+		String mName = in.nextLine();
+		
+		System.out.print("Please enter your last name: ");
+		String lName = in.nextLine();
+		in.close();
+		
+		System.out.println("Your initials are " + fName.charAt(0) + mName.charAt(0) + lName.charAt(0) + ".");
 	}
 }
